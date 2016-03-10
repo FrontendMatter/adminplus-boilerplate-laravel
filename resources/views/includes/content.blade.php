0d4c3cc83d962b0c8a951b3e45fdf07ee7b0a256
Laza Bogdan <laza.bogdan@macro-net.ro>
2016-03-10 22:20:35 +0200

    initial commit

---
+++ resources/views/includes/content.blade.php
@@ -0,0 +1,10 @@
+<h1>Hello World</h1>
+
+<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae ea ullam iusto asperiores repellat perspiciatis error. Quo praesentium, expedita neque natus quisquam iure consequuntur unde hic doloribus ab voluptas pariatur!</p>
+
+<div class="card">
+    <div class="card-block">
+        This is a demo for the <a target="_blank" href="https://github.com/themekit/adminplus-boilerplate-laravel">adminplus-boilerplate-laravel</a> repository. <br>
+        Read the full guide <a target="_blank" href="http://adminplus.themekit.io/laravel-bootstrap-theme">Build a Laravel app with AdminPlus Lite Bootstrap Theme</a>.
+    </div>
+</div>
